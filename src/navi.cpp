d7945af7d2c7b570769f2f8d9389a6ba16a9ca3c
Luiserebii <luis@serebii.io>
2019-10-24 00:36:06 -0400

    Idea on how to handle fill()

--- src/navi.cpp
+++ src/navi.cpp
@@ -5,6 +5,32 @@ namespace Navi {
 
 
 /**
+ * In the spirit of std::accumulate using type-hinting via
+ * the third parameter to determine what to call, we
+ * will be doing something similar for fill by taking
+ * advantage of this piece of templating.
+ *
+ * Alternate solution in mind was to use enums; have the
+ * user pass a particular one, representing a type, to
+ * figure out how to process the rest, for example:
+ *
+ * fill(v.begin(), v.end(), Navi::INT)
+ *
+ * Actually, I think I will use the enum solution, and
+ * simply keep this for filling a container up with a value
+ *
+ * Ahhhh! An alternate solution could be to have a user pass
+ * a function in, maybe... let's keep it simple
+ */
+template <class Out> 
+Out fill(Out begin, Out end, T val) {
+    while(begin != end) {
+        *begin++ = val;
+    }
+    return begin;
+}
+
+/**
  * toString function for all containers
  */
 template <class In>
