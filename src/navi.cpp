aa9a688a3cb67234567c6a5d48b0d7f3ae66e2c8
Luiserebii <luis@serebii.io>
2019-10-24 00:57:57 -0400

    Fleshing out fill implementations, especially the enum-based ones

--- src/navi.cpp
+++ src/navi.cpp
@@ -1,8 +1,14 @@
 #include <string>
 #include <sstream>
+#include <cstdlib>
+
+using std::string;
+using std::stringstream;
+using std::rand;
 
 namespace Navi {
 
+enum class Type { INT, STRING }
 
 /**
  * In the spirit of std::accumulate using type-hinting via
@@ -10,14 +16,7 @@ namespace Navi {
  * will be doing something similar for fill by taking
  * advantage of this piece of templating.
  *
- * Alternate solution in mind was to use enums; have the
- * user pass a particular one, representing a type, to
- * figure out how to process the rest, for example:
- *
- * fill(v.begin(), v.end(), Navi::INT)
- *
- * Actually, I think I will use the enum solution, and
- * simply keep this for filling a container up with a value
+ * Simply keep this for filling a container up with a value
  *
  * Ahhhh! An alternate solution could be to have a user pass
  * a function in, maybe... let's keep it simple
@@ -29,6 +28,38 @@ Out fill(Out begin, Out end, T val) {
     }
     return begin;
 }
+/**
+ * Alternate solution in mind was to use enums; have the
+ * user pass a particular one, representing a type, to
+ * figure out how to process the rest, for example:
+ *
+ * fill(v.begin(), v.end(), Navi::INT)
+ */
+template <class Out>
+Out fill(Out begin, Out end, Type t) {
+    if(t == Type::INT) {
+        return fillInt(begin, end);
+    } else if(t == Type::STRING) {
+        return fillString(begin, end);
+    }
+}
+
+template <class Out>
+Out fillInt(Out begin, Out end, int lim = 100) {
+    while(begin != end) {
+        *begin++ = rand() % lim;
+    }
+    return begin;
+}
+
+template <class Out>
+Out fillString(Out begin, Out end) {
+    while(begin != end) {
+        *begin++ = "Default String";
+    }
+    return begin;
+}
+
 
 /**
  * toString function for all containers
